a44148199a9015bff700ed983bb17bf21c9b25a5
Raghunath <raghunathnv2020@gmail.com>
2020-04-26 19:58:04 -0400

    commit 3

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,7 +14,6 @@ public class Greeter {
 
   //TODO: Add javadoc comment
   public String greet(String someone) {
-	  System.out.println("commit1");
 	  System.out.println("commit2");
     return String.format("Hello123, %s!", someone);
   }
